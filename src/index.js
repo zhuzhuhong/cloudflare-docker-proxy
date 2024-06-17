4fa1f2f7a03ac28fbba4ef53afdcfaf65034ce9c
祝小默默 <745729377@qq.com>
2024-06-17 12:14:02 +0800

    更新相关信息

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.934562.xyz": dockerHub,
+  "quay.934562.xyz": "https://quay.io",
+  "gcr.934562.xyz": "https://gcr.io",
+  "k8s-gcr.934562.xyz": "https://k8s.gcr.io",
+  "k8s.934562.xyz": "https://registry.k8s.io",
+  "ghcr.934562.xyz": "https://ghcr.io",
+  "cloudsmith.934562.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.934562.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
